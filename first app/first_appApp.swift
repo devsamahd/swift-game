db90c2ee40419edbc25d41efe30c22034a74c90a
devsamahd <samahd626@gmail.com>
2023-08-30 11:42:39 +0100

    Initial Commit

---
+++ first app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first app
+//
+//  Created by USER on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
